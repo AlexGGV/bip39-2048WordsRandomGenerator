92892cc6c5af11fc6c32ffe134b8a1157a378f81
Michael Chevallier <michael.chevallier@gmail.com>
2018-03-13 15:11:22 +0100

    done

---
+++ listmaker.py
@@ -0,0 +1,24 @@
+import os, sys, datetime, random
+
+#option to allow seed settings for backup purpose
+if (sys.argv == 2):
+	random.seed(sys.argv[1])
+#open words list
+wordlist = open("bip39.txt", "r")
+#create output file
+directory = "output"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+loc = directory + "/" + str(datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')) + ".txt"
+fd = open (loc, "w")
+arr = []
+
+for line in wordlist:
+	arr.append(line.strip('\n'))
+for i in range (0, 2048):
+	if (i % 5 == 0 and i != 0):
+		fd.write("\n")
+	curword = random.randrange(0, 2047 - i + 1)
+	current = ("[%4d]%8s " % (i + 1, arr[curword]))
+	arr.remove(arr[curword])
+	fd.write(current)
